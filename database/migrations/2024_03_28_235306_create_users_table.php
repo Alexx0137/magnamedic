500225036c1ded4af5bbbd82f7f7100d36fee273
Alexx0137 <alexx0137@hotmail.com>
2024-08-05 16:02:26 -0500

    avance migration 2

--- database/migrations/2024_03_28_235306_create_users_table.php
+++ database/migrations/2024_03_28_235306_create_users_table.php
@@ -4,26 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateUsersTable extends Migration
 {
-    public function up(): void
+    public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('identification_type_id')->constrained('identification_types');
+            $table->foreignId('identification_type_id')->constrained()->onDelete('cascade');
             $table->string('identification', 45);
             $table->string('name', 45);
             $table->string('last_name', 45)->nullable();
             $table->string('email', 100)->nullable();
-            $table->foreignId('role_id')->constrained('roles');
+            $table->foreignId('role_id')->constrained()->onDelete('cascade');
             $table->string('password', 100)->nullable();
             $table->integer('state');
             $table->timestamps();
         });
     }
 
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('users');
     }
-};
+}
